bca7e1856e312ea9a251f0f9c76ea2372b78e242
Robin Dickson <robindickson@gmail.com>
2015-05-30 17:07:24 +0200

    Improved style of hash and array.

--- blackjack.rb
+++ blackjack.rb
@@ -20,17 +20,12 @@
 require 'pry'
 
 def initialize_game_data
-  game_data = { :player => { :hand => [], :score => 0 },
-            :dealer => { :hand => [], :score => 0 },
-            :deck => ['AH', '2H', '3H', '4H', '5H', '6H',
-                      '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH',
-                      'AD', '2D', '3D', '4D', '5D', '6D',
-                      '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD',
-                      'AC', '2C', '3C', '4C', '5C', '6C',
-                      '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC',
-                      'AS', '2S', '3S', '4S', '5S', '6S',
-                      '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS'] }
-
+  game_data = { player: { hand: [], score: 0 },
+                dealer: { hand: [], score: 0 },
+                deck: %w(AH 2H 3H 4H 5H 6H 7H 8H 9H 10H JH QH KH
+                         AD 2D 3D 4D 5D 6D 7D 8D 9D 10D JD QD KD
+                         AC 2C 3C 4C 5C 6C 7C 8C 9C 10C JC QC KC
+                         AS 2S 3S 4S 5S 6S 7S 8S 9S 10S JS QS KS) }
 
   values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
             11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
@@ -123,13 +118,13 @@ def one_game
   initial_deal(game_data)
 
   if game_data[:player][:score] == 21
-    "You win!"
+    puts "You win!"
 
   else
 
     players_turn(game_data)
     if game_data[:player][:score] > 21
-      puts "You have busted!"
+      puts "You have busted Dealer wins!"
     elsif game_data[:player][:score] == 21
       puts "You win!"
     else
